f5ce633ff44f9f850246c69e3ae753fa57352013
Kim TaeHyeong <kysth0707@naver.com>
2022-10-10 18:29:33 +0900

    docs : function description added.

--- ShowModule.py
+++ ShowModule.py
@@ -1,3 +1,18 @@
 def ShowResult(Texts, Times):
+	"""
+	Parameters
+	----------
+	Texts : list (str)
+		The name of Time list
+	Times : list (number)
+		The time of Function times
+
+	Output
+	----------
+	Texts[0]        Times[0]  \n
+	Texts[1]        Times[1]  \n
+	...
+	(Loop Texts Length)
+	"""
 	for i in range(len(Texts)):
 		print(f"{Texts[i]}\t\t{Times[i]}")
